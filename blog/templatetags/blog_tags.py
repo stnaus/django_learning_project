ae9e13e9924edae9967fdb5902210ad0aba5a1ef
Stanislav Gusev <st.al.gusev@gmail.com>
2023-09-16 22:21:01 +0300

    Add template filter and sitemap

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.db.models import Count
 from ..models import Post
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -20,3 +22,7 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(
         total_comments=Count('comments')
     ).order_by('-total_comments')[:count]
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
